086dc43c7e390c05d4bbac8ea236158ab4efabaf
XiaFF <xiaff@users.noreply.github.com>
2019-03-15 11:29:56 +0800

    add error handling

--- src/main/java/wiki/label/WikiDataQueryService.java
+++ src/main/java/wiki/label/WikiDataQueryService.java
@@ -6,6 +6,8 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import wiki.label.dto.WikiLabelDTO;
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
 
 @Component
 public class WikiDataQueryService {
+  public static final Logger logger = LoggerFactory.getLogger(WikiDataQueryService.class);
   private static final int DEFAULT_QUERY_SIZE = 50;
   private static OkHttpClient CLIENT = new OkHttpClient();
   private static Random RANDOM = new Random();
@@ -37,34 +40,40 @@ public class WikiDataQueryService {
 
     for (List<String> subLabelList : partition(labels)) {
       String queryIds = StringUtils.join(subLabelList, "|");
-      System.out.println(queryIds);
+      System.out.println(StringUtils.abbreviate(queryIds, 60));
       List<WikiLabelDTO> wikiLabelDTOS = queryWikiTitle(queryIds);
+      if (wikiLabelDTOS == null) {
+        // wait for 30s if query failed
+        logger.warn("Sleeping for 30 sec...");
+        Thread.sleep(1000 * 30);
+        wikiLabelDTOS = queryWikiTitle(queryIds);
+        if (wikiLabelDTOS == null) {
+          wikiLabelDTOS = Collections.emptyList();
+        }
+      }
       wikiLabelDTOList.addAll(wikiLabelDTOS);
-      Thread.sleep((long) (1000 * RANDOM.nextDouble()));
+      Thread.sleep((long) (500 * RANDOM.nextDouble()));
     }
     return wikiLabelDTOList;
   }
 
-  private List<WikiLabelDTO> queryWikiTitle(String queryLabelStr) throws IOException {
+  private List<WikiLabelDTO> queryWikiTitle(String queryLabelStr) {
     List<WikiLabelDTO> wikiLabelDTOList = new ArrayList<>();
 
     Request request = new Request.Builder().url("https://www.wikidata.org/w/api.php?action=wbgetentities&props=labels&ids=" + queryLabelStr + "&format=json&languages=en|zh-cn").get().build();
     try (Response response = CLIENT.newCall(request).execute()) {
       String result = response.body().string();
-//      System.out.println(result);
       JSONObject jsonObject = JSON.parseObject(result);
-//      System.out.println(jsonObject);
       boolean success = jsonObject.getBoolean("success");
       if (!success) {
-        return Collections.emptyList();
+        logger.error("Query unsuccessful: {}", jsonObject);
+        return null;
       }
-      System.out.println("success: " + success);
       JSONObject entitiesJson = jsonObject.getJSONObject("entities");
       for (Map.Entry<String, Object> stringObjectEntry : entitiesJson.entrySet()) {
         String mLabel = stringObjectEntry.getKey();
         WikiLabelDTO wikiLabelDTO = new WikiLabelDTO(mLabel);
 
-//        System.out.print(mLabel + ": ");
         JSONObject valueJson = (JSONObject) stringObjectEntry.getValue();
         JSONObject labelsJson = valueJson.getJSONObject("labels");
         if (labelsJson == null) {
@@ -76,15 +85,16 @@ public class WikiDataQueryService {
           String titleEnLower = StringUtils.lowerCase(titleEn);
           wikiLabelDTO.setTitleEn(titleEn);
           wikiLabelDTO.setTitleEnLower(titleEnLower);
-//          System.out.print(titleEn + ", " + titleEnLower + ", ");
         }
         if (labelsJson.containsKey("zh-cn")) {
           String titleCn = labelsJson.getJSONObject("zh-cn").getString("value");
           wikiLabelDTO.setTitleCn(titleCn);
-//          System.out.println(titleCn);
         }
         wikiLabelDTOList.add(wikiLabelDTO);
       }
+    } catch (IOException e) {
+      logger.warn("Http error: {}.", e.getMessage());
+      return null;
     }
 
     return wikiLabelDTOList;
